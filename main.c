05551958b5e8c65a47f75c322caea47d711ec3a1
EdgarRG <edgarrg242002@gmail.com>
2025-09-13 22:21:47 -0600

    Generador y Suma (Incorrectos)

--- main.c
+++ main.c
@@ -1,41 +1,80 @@
 #include<stdio.h>
+#include <stdlib.h> 
 
+int NumeroDeVectores = 100;
+int Quantum=10;
+int i=0;
+int NumeroDeVueltas=0;
+struct Vector{
+    float Magnitud;
+    float Angulo;
+
+}Vector1[100],Vector2[100],Suma[100],Resta[100];
 
 
 int main(){
 
+    
+GenerarVectores();
+SumaVectores();
+//while ()
+//{
+    /* code */
+//}
 
-
-}
-
-int Manitud(){
-
-
-}
-
-
-int RestaVectores(){
 
 
 }
+
+int GenerarVectores(){
+	for(i=0;i<NumeroDeVectores;i++){
+    	
+
+    	
+
+    	Vector1[NumeroDeVectores].Magnitud=(rand())+1;
+    	Vector1[NumeroDeVectores].Angulo=(rand())+1;
+    	Vector2[NumeroDeVectores].Magnitud=(rand())+1;
+    	Vector2[NumeroDeVectores].Angulo=(rand())+1;
+		printf("Vector 1 %d Magnitud: %f\n",i,Vector1[NumeroDeVectores].Magnitud);
+    	printf("Vector 1 %d Angulo: %d\n",i,Vector1[NumeroDeVectores].Angulo);
+    	printf("Vector 2 %d Magnitud: %f\n",i,Vector2[NumeroDeVectores].Magnitud);
+    	printf("Vector 2 %d Angulo: %d\n",i,Vector2[NumeroDeVectores].Angulo);
+    }
+
+}
+
+//int Magnitud(){
+
+
+//}
+
+
+//int RestaVectores(){
+
+
+//}
 
 int SumaVectores(){
-
-
-
-}
-
-int ProductoEscalarVectores(){
-
+	
+	Suma[1].Magnitud=Vector1[1].Magnitud+Vector2[1].Magnitud;
+	Suma[1].Angulo=Vector1[1].Angulo+Vector2[1].Angulo;
+    printf("Vector Suma Magnitud: %f\n",Suma[1].Magnitud);
+    printf("Vector Suma Angulo: %d\n",i,Suma[1].Angulo);
 
 }
 
+//int ProductoEscalarVectores(){
 
-int EscalarVector(){
+
+//}
+
+
+//int EscalarVector(){
 
 
     
-}
+//}
 
 
 
