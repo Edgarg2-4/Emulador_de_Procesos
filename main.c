d5a41a08a9b7c309d58d091fa5adb32816827965
EdgarRG <edgarrg242002@gmail.com>
2025-09-15 07:59:37 -0600

    Primer Control

--- main.c
+++ main.c
@@ -2,22 +2,29 @@
 #include <stdlib.h> 
 #include <time.h>
 
-int NumeroDeVectores = 100;
+int NumeroDeVectores=100;
 int Quantum=10;
+int *Proceso;
+int NumeroDeTick=0;
 int i=0;
 int NumeroDeVueltas=0;
 struct Vector{
     float Magnitud;
     float Angulo;
+	//No se donde tengamos que utilizar malloc
+	//No estoy seguro de como utilizar los apuntadores
+	//Tal Vez con un Apuntador a un arreglo
 
-}Vector1[100],Vector2[100],Suma[100],Resta[100];
+}Vector1[100],Vector2[100],Suma[100],Resta[100]; //Forma Rectangular
 
 
 int main(){
-
+	
 srand(time(NULL)); // inicializa la semilla
 GenerarVectores();
 SumaVectores();
+RestaVectores();*/
+printf("Se acabo Planificador");
 //while ()
 //{
     /* code */
@@ -45,27 +52,40 @@ int GenerarVectores(){
 
 }
 
-//int Magnitud(){
-
+//int MagnitudDeVectores(){
+//Raiz de del cuadrado de la resta magnitud y resta del angulo
 
 //}
 
 
 int RestaVectores(){
-	Resta[1].Magnitud=Vector1[1].Magnitud-Vector2[1].Magnitud;
-	Resta[1].Angulo=Vector1[1].Angulo-Vector2[1].Angulo;
-    printf("Vector Resta Magnitud: %f\n",Resta[1].Magnitud);
-    printf("Vector Resta Angulo: %f \n",Resta[1].Angulo);
+	for(i=0;i<NumeroDeVectores;i++){
+		Resta[i].Magnitud=Vector1[i].Magnitud-Vector2[i].Magnitud;
+		Resta[i].Angulo=Vector1[i].Angulo-Vector2[i].Angulo;
+		NumeroDeTick++;
+		printf("Vector Resta %d Magnitud: %f\n",i,Resta[i].Magnitud);
+		if(NumeroDeTick==10){
+			//Antes de salir hay que guardar la i para llegar el siguiente valor en la i correspondiente
+			printf("Antes de Break Vector Resta %d Magnitud: %f\n",i,Resta[i].Magnitud);
+			NumeroDeTick=0;
+			break;
+		}
+	}
 
 }
 
 int SumaVectores(){
-	
-	Suma[1].Magnitud=Vector1[1].Magnitud+Vector2[1].Magnitud;
-	Suma[1].Angulo=Vector1[1].Angulo+Vector2[1].Angulo;
-    printf("Vector Suma Magnitud: %f\n",Suma[1].Magnitud);
-    printf("Vector Suma Angulo: %f \n",Suma[1].Angulo);
-
+	for(i=0;i<NumeroDeVectores;i++){
+		Suma[i].Magnitud=Vector1[i].Magnitud+Vector2[i].Magnitud;
+		Suma[i].Angulo=Vector1[i].Angulo+Vector2[i].Angulo;
+    	NumeroDeTick++;
+		printf("Vector Suma %d Magnitud: %f\n",i,Suma[i].Magnitud);
+		if(NumeroDeTick==10){
+			printf("Antes de Break Vector Suma %d Magnitud: %f\n",i,Suma[i].Magnitud);
+			NumeroDeTick=0;
+			break;
+		}
+	}
 }
 
 //int ProductoEscalarVectores(){
