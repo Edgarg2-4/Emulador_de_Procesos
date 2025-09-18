8e648bab50a7f8e9d205d35818e75a60074a311c
Maximiliano Martinez <matsmaco@gmail.com>
2025-09-18 01:08:15 -0600

    Se ha verificado el correcto funcionamiento

--- main.c
+++ main.c
@@ -15,10 +15,10 @@ int p = 0;
 int NumeroDeVueltas = 0;
 int Escalar = 4;
 
-int V1[100],V2[100],R_Suma[100], R_Resta[100];
+int V1[100], V2[100], R_Suma[100], R_Resta[100];
 float R_Modulo[100], R_Magnitud[100];
-int R_Escalar1[100],R_Escalar2[100];
-int R_Producto=0;
+int R_Escalar1[100], R_Escalar2[100];
+int R_Producto = 0;
 int Prioridades[5];
 
 
@@ -27,29 +27,25 @@ void GenerarVectores(){
 	for(i = 0 ; i < NumeroDeVectores ; i++){
     	V1[i]=(rand() % 100) + 1;   
     	V2[i]=(rand() % 100) + 1;   	
-	//	printf("%d. Vector 1 x = %d, y = %d\n",i,V1[i].x, V1[i].y);
-    //	printf("%d. Vector 2 x = %d, y = %d\n",i,V2[i].x, V2[i].y);
+		printf("%d. Vector 1  = %d\n",i,V1[i]);
+    	printf("%d. Vector 2  = %d\n",i,V2[i]);
     }
 
 }
 
 void Resultados(){
-	while(p!=k){
-	printf("Suma = %d\n",R_Suma[i]);
-	printf("Resta = %d\n",R_Resta[i]);
-	printf("Magnitud = %f\n",R_Magnitud[i]);
-	printf("Producto Escalar = %d\n",R_Producto);
-	printf("Vector1 por Escalar = %d\n",R_Escalar1[i]);
-	printf("Vector2 Por Escalar = %d\n",R_Escalar2[i]);
-	p++;
-}
-p=k;
-if(k==100){
-	p=0;
-
-}
-	sleep(2);
-
+	while(p != i){
+		printf("Suma = %d\n",R_Suma[p]);
+		printf("Resta = %d\n",R_Resta[p]);
+		printf("Magnitud = %f\n",R_Magnitud[p]);
+		printf("Producto Escalar = %d\n",R_Producto);
+		printf("Vector1 por Escalar = %d\n",R_Escalar1[p]);
+		printf("Vector2 Por Escalar = %d\n",R_Escalar2[p]);
+		p++;
+	}	 
+	if(i == 100){
+		p = 0;
+	}
 }
 
 
@@ -72,7 +68,7 @@ void llenarPrioridades(){
 			k++;
 		}
 	}
-	printf("[%d, %d, %d, %d, %d] \n", Prioridades[0],Prioridades[1],Prioridades[2],Prioridades[3],Prioridades[4]);
+	//printf("[%d, %d, %d, %d, %d] \n", Prioridades[0],Prioridades[1],Prioridades[2],Prioridades[3],Prioridades[4]);
 }
 
 void SumaVectores(int i){
@@ -96,15 +92,15 @@ void ProductoVectores(int i){
 }
 
 void EscalarVectores(int i){
-	R_Escalar1[i]=Escalar*V1[i];
-	R_Escalar2[i]=Escalar*V2[i];
+	R_Escalar1[i] = Escalar * V1[i];
+	R_Escalar2[i] = Escalar * V2[i];
 }
 
 int main(void){
 	srand((unsigned)time(NULL)); // inicializa la semilla
 	GenerarVectores();
 	
-	void (*ops[])(int) = {SumaVectores, RestaVectores, ProductoVectores, MagnitudVectores,};
+	void (*ops[])(int) = {SumaVectores, RestaVectores, MagnitudVectores, ProductoVectores, EscalarVectores};
 	i = 0;
 	while(1){
 		llenarPrioridades();
@@ -113,26 +109,14 @@ int main(void){
 				ops[Prioridades[j]](k + i);
 			}
 		}
-		i = k;
+		i += k;
+		sleep(3);
 		Resultados();
 		if(i == 100){
-			//delay
 			i = 0;
 			GenerarVectores();
-			
 		} 
-		
 	}
-	
-	
-	/*while(i < 100){
-		ops[0](i);
-		ops[1](i);
-		i++;
-	}
-	for(j = 0 ; j < 10 ; j++){
-		llenarPrioridades();
-	}*/
 	return 0;
 }
 
