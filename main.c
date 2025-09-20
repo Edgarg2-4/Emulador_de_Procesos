77cca997617305c908d7f189cdb7a76bcc8dc3e2
DerianRdd <95389466+DerianRdd@users.noreply.github.com>
2025-09-19 18:13:45 -0600

    Update main.c

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ int k = 0;                    // Variable para recorrer los 10 ticks
 int p = 0;                   // Variable para recorrer índices impresos
 int NumeroDeVueltas = 0;
 int Escalar = 4;        //Variable para el escalador
+int pos = 0;
+int val = 0;
 
 // Vectores de entrada
 int V1[100],V2[100];
@@ -59,7 +61,7 @@ void GenerarVectores(){
 	}
 }
 
-//Funcion que imprime resultados parciales de cada proceso
+//Funcion que imprime resultados parciales de cada proceso -------------------- ya no se usa ---------------------------------
 void Resultados(){
 
 	printf("------PROCESO %d con prioridades [%d, %d, %d, %d, %d]------\n", i/10, Prioridades[0],Prioridades[1],Prioridades[2],Prioridades[3],Prioridades[4]);
@@ -142,28 +144,93 @@ void LlenarPrioridades(){
 			k++;
 		}
 	}
+	printf("------PROCESO %d con prioridades [%d, %d, %d, %d, %d]------\n", i/10, Prioridades[0],Prioridades[1],Prioridades[2],Prioridades[3],Prioridades[4]);
 }
 
 // ---------------------------- Procesos --------------------------------------------------
 void SumaVectores(int i){
 	R_Suma[i] = V1[i] + V2[i];
+	
+	if(k==Quantum-1){
+	    printf("Suma:\n[");
+	    for(p = i - Quantum; p < i ; p++){
+    	if(p == i - 1){
+    		printf("%d]\n\n", R_Suma[p]);
+		}
+		else{
+			printf("%d, ", R_Suma[p]);
+		}
+	}
+	}
+	
 }
 
 void RestaVectores(int i){
 	R_Resta[i] = V1[i] - V2[i];
+	
+	if(k==Quantum-1){
+	    printf("Resta:\n[");
+	    for(p = i - Quantum; p < i ; p++){
+    	if(p == i - 1){
+    		printf("%d]\n\n", R_Resta[p]);
+		}
+		else{
+			printf("%d, ", R_Resta[p]);
+		}
+	}
+	}
 }
 
 void MagnitudVectores(int i){
 	R_Magnitud[i] = sqrt(pow(V1[i],2)+pow(V2[i],2));
+	if(k==Quantum-1){
+	    printf("Magnitud:\n[");
+	    for(p = i - Quantum; p < i ; p++){
+    	    if(p == i - 1){
+    		    printf("%.2f]\n\n", R_Magnitud[p]);
+		    }
+		    else{
+		    	printf("%.2f]\n\n", R_Magnitud[p]);
+		    }
+	    }
+	}
 }
 
 void ProductoVectores(int i){
 	R_ProductoPunto += (V1[i]*V2[i]); // codigo nuevo: acumulador usa R_ProductoPunto
+	
+	if(k==Quantum-1){
+	    printf("Producto Escalar:\n[ %d ",R_ProductoPunto); 
+	    printf("]\n\n");
+	}
 }
 
 void EscalarVectores(int i){
 	R_Escalar1[i]=Escalar*V1[i];
 	R_Escalar2[i]=Escalar*V2[i];
+	
+	if(k==Quantum-1){
+	    printf("Vector 1 por Escalar:\n[");
+	    for(p = i - Quantum; p < i ; p++){
+    	    if(p == i - 1){
+    		    printf("%d]\n\n", R_Escalar1[p]);
+		    }
+		    else{
+		    	printf("%d, ", R_Escalar1[p]);
+		    }
+	    }
+	    printf("Vector 2 por Escalar:\n[");
+	    for(p = i - Quantum; p < i ; p++){
+    	    if(p == i - 1){
+    		    printf("%d]\n\n", R_Escalar2[p]);
+		    }
+		    else{
+		    	printf("%d, ", R_Escalar2[p]);
+		    }
+	    }
+	}
+	
+	
 }
 
 // -------------------------- MAIN -----------------------------------------
@@ -177,19 +244,44 @@ int main(void){
 	i = 0;
 	while(1){  // iniciamos bucle infinito
 		LlenarPrioridades(); // asignamos orden de prioridad aleatorio a cada proceso
-		for(j = 0 ; j < 5 ; j++){ // recorremos los 5 procesos en el orden de prioridad asignado
-			for(k = 0 ; k < Quantum ; k++){ // ejecutamos cada proceso durante 10 quantums
-				ops[Prioridades[j]](k + i);
-			}
-		}
-		i += k; //actualiza valor de i
-		sleep(3); // pausa para ver resultados
-		Resultados(); //Imprimimos resultados obtenidos
-		if(i == 100){
-			i = 0;
-			GenerarVectores(); //Genera nuevos vectores V1 y v2 cuando se han completado todos lor procesos.
-		} 
-	}
-	
+		// --------------------------------------- añadi esto
+		val = 0;
+		pos = 0;
+		while(val <= 4){ // ejecuta los 5 procesos
+		//printf("Valor val: %d \n",val);
+		//printf("Valor pos: %d \n",pos);
+		//printf("Prioridad[pos]: %d \n",Prioridades[pos]);
+		    if(Prioridades[pos] == val){ // revisamos si el valor de la posicion es igual al valor de prioridad
+		        for(k = 0 ; k < Quantum ; k++){ // ejecutamos cada proceso durante 10 quantums
+    				ops[pos](k + i); // una vez que se encuentra en la posicion correcta, ejecuta ese proceso
+    			}
+    		    if(val > 4){ //Reseteamos val si se excede de numero de prioridades
+		            val = 0;
+		        }else{
+		            val++;
+		        }
+    			if(pos > 4){//Reseteamos pos si se excede de numero de posiciones
+		            pos = 0;
+		        }else{
+		            pos++;
+		        }
+		        i += k; //actualiza valor de i
+    		    sleep(1); // pausa para ver resultados
+    		    //Resultados(); //Imprimimos resultados obtenidos
+    			
+		    }else{
+		        if(pos > 4){ // si la posicion excede el numero de elementos del vector, se reinicia
+		            pos = 0;
+		        }else{
+		            pos++;
+		        }
+		    }
+		} // cierre de while( val < 5)
+        if(i == 100){
+    		i = 0;
+    		GenerarVectores(); //Genera nuevos vectores V1 y v2 cuando se han completado todos lor procesos.
+    		R_ProductoPunto = 0;
+    	} 
+	} // cierre del while(1)
 	return 0;
 }
